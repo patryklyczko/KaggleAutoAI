78ab56de696358b36a6b58d32a224735fec4ca15
patryklyczko <lyczkopatryk1@gmail.com>
2023-05-12 22:50:39 +0200

    feat: automated ai & package elements

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from .model_data import *
 from .classification import *
 from .regression import *
+from .automated_ai import *
 
 __all__ = [
     'model_data'
     'classification',
-    'regression',
+    'regression_automated',
+    "automated_ai"
 ]
