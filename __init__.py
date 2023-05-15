0551a197338982da05246350948979e38ec82e69
patryklyczko <lyczkopatryk1@gmail.com>
2023-05-15 14:39:43 +0200

    feat: init

--- __init__.py
+++ __init__.py
@@ -2,10 +2,12 @@ from .model_data import *
 from .classification import *
 from .regression import *
 from .automated_ai import *
+from .language_processing import *
 
 __all__ = [
     'model_data'
     'classification',
     'regression_automated',
     "automated_ai"
+    "language_processing"
 ]
